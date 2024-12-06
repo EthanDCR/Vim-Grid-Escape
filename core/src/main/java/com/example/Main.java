0f75de2a059e2cee58b9eba03785ac129ba9b734
EthanDCR <clinteth.dev@gmail.com>
2024-12-06 17:43:14 -0600

    bug fixes

--- core/src/main/java/com/example/Main.java
+++ core/src/main/java/com/example/Main.java
@@ -8,76 +8,74 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/**
- * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
- * platforms.
- */
 public class Main extends ApplicationAdapter {
-    private int desiredwidth;
-    private int desiredheight;
     private SpriteBatch batch;
     private Texture cursor;
     private Texture background;
     private Texture alien;
     private Texture bullet;
-    float x = 50;
-    float y = 50;
+
+    private float playerX = 300;
+    private float playerY = 50;
+    private float bulletX = -1; // -1 indicates no active bullet
+    private float bulletY = -1;
+    private final int screenWidth = 1920;
+    private final int screenHeight = 1080;
 
     @Override
     public void create() {
+        background = new Texture(Gdx.files.internal("spacebackground.jpg"));
+        cursor = new Texture(Gdx.files.internal("Untitled.png"));
+        alien = new Texture(Gdx.files.internal("amogus.gif"));
+        bullet = new Texture(Gdx.files.internal("PinkBullet.png"));
         batch = new SpriteBatch();
-        background = new Texture(Gdx.files.internal("pinkspace.jpg"));
-        cursor = new Texture(Gdx.files.internal("ship.png"));
-        alien = new Texture(Gdx.files.internal("alien.png"));
-        bullet = new Texture(Gdx.files.internal("firebullet.png"));
-
-        desiredwidth = Gdx.graphics.getWidth();
-        desiredheight = Gdx.graphics.getHeight();
-
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        handlePlayerMovement();
+
+        handleShooting();
+
         batch.begin();
-        batch.draw(background, 0, 0);
-        batch.draw(alien, 1600, 1500);
+        batch.draw(background, 0, 0, screenWidth, screenHeight);
+        batch.draw(cursor, playerX, playerY);
+
+        if (bulletY > -1) { // Draw bullet if active
+            batch.draw(bullet, bulletX, bulletY);
+        }
+
+        batch.draw(alien, 250, 250); // Temporary static alien position
         batch.end();
-        y = 10;
-
-        if (Gdx.input.isKeyPressed(Keys.H)) {
-            x = x - 16;
-        } else if (Gdx.input.isKeyPressed(Keys.L)) {
-            x = x + 16;
-        } else if (Gdx.input.isKeyPressed(Keys.E) || Gdx.input.isKeyPressed(Keys.W)) {
-            x = x + 32;
-        } else if (Gdx.input.isKeyPressed(Keys.B)) {
-            x = x - 32;
-        }
-
-        if (Gdx.input.isKeyPressed(Keys.H)) {
-            x = x - 16;
-        } else if (Gdx.input.isKeyPressed(Keys.L)) {
-            x = x + 16;
-        } else if (Gdx.input.isKeyPressed(Keys.E) || Gdx.input.isKeyPressed(Keys.W)) {
-            x = x + 32;
-        } else if (Gdx.input.isKeyPressed(Keys.B)) {
-            x = x - 32;
-        }
-
-        if (x > 3839) {
-            x = 1;
-        } else if (x < 1) {
-            x = 3839;
-        }
-
     }
 
-    public void shoot() {
-        if (Gdx.input.isKeyPressed(Keys.X)) {
+    private void handlePlayerMovement() {
+        if (Gdx.input.isKeyPressed(Keys.H)) {
+            playerX -= 16;
+        } else if (Gdx.input.isKeyPressed(Keys.L)) {
+            playerX += 16;
+        }
 
-            batch.draw(bullet, x, );
+        // Wrap-around logic
+        if (playerX > screenWidth) {
+            playerX = 0;
+        } else if (playerX < 0) {
+            playerX = screenWidth;
+        }
+    }
 
+    private void handleShooting() {
+        if (Gdx.input.isKeyJustPressed(Keys.K) && bulletY == -1) { // Fire new bullet
+            bulletX = playerX + cursor.getWidth() / 2 - bullet.getWidth() / 2;
+            bulletY = playerY + cursor.getHeight();
+        }
+
+        if (bulletY > -1) {
+            bulletY += 20;
+            if (bulletY > screenHeight) {
+            }
         }
     }
 
@@ -85,5 +83,9 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         cursor.dispose();
+        background.dispose();
+        alien.dispose();
+        bullet.dispose();
     }
 }
+
